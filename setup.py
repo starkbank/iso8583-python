f2f3ecf25ac426db67782747b5036f5ee5745f8e
Delson Barros <delson.barros@starkbank.com>
2025-09-02 18:06:44 -0300

    Update to v0.6.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     author="Stark Bank",
     author_email="developers@starkbank.com",
     keywords=["iso8583", "credit card", "debit card", "mastercard", "visa"],
-    version="0.6.0"
+    version="0.6.1"
 )
 
 """
